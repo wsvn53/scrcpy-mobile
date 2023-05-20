344e64c9148ba23653d7a47a7c410213bbc43677
ethan <wsvn53@gmail.com>
2023-05-20 11:55:45 +0800

    Modify scrcpy options and defaults

--- scrcpy-ios/scrcpy-ios/ScrcpyClient.m
+++ scrcpy-ios/scrcpy-ios/ScrcpyClient.m
@@ -440,12 +440,14 @@ void ScrcpyHandleFrame(AVFrame *frame) {
 #pragma mark - Scrcpy Options
 
 -(NSArray *)defaultScrcpyOptions {
-    return @[ @"--verbosity=debug", @"--shortcut-mod=lctrl+rctrl", @"--fullscreen", @"--display-buffer=32",
-              @"--video-bit-rate=4M", @"--audio-bit-rate=128K", @"--audio-buffer=100", @"--max-fps=60", @"--print-fps", ];
+    return @[ @"--verbosity=debug", @"--shortcut-mod=lctrl+rctrl", @"--fullscreen", @"--display-buffer=10",
+              @"--video-bit-rate=4M", @"--audio-bit-rate=128K", @"--max-fps=60", @"--print-fps", ];
 }
 
 -(NSArray *)availableOptions {
-    return @[ @"max-size", @"video-bit-rate", @"disable-screensaver", @"display-buffer",
+    return @[ @"max-size", @"video-bit-rate", @"audio-bit-rate", @"audio-buffer",
+              @"audio-codec", @"no-audio",
+              @"disable-screensaver", @"display-buffer",
               @"force-adb-forward", @"max-fps", @"power-off-on-close", @"turn-screen-off",
               @"show-touches", @"stay-awake", ];
 }
